6da1d74567d27dce3ecdc82b476e2801db90841a
theribaby123 <32952496+theribaby123@users.noreply.github.com>
2017-10-21 13:34:30 +0530

    Add files via upload

---
+++ python_players/len.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse015
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse015 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    string="programmer"
+    print len(string)
+
+if __name__ == '__main__':
+    main()
