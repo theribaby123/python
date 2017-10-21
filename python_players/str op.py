26db5cb2a8137fe12563bf98aaab2f25564d34c6
theribaby123 <32952496+theribaby123@users.noreply.github.com>
2017-10-21 13:49:31 +0530

    Add files via upload

---
+++ python_players/str op.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse015
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse015 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    string="programmer"
+    print(string.count('r'))
+    print(string.find('g'))
+    print(string.replace('m','y'))
+
+
+if __name__ == '__main__':
+    main()
